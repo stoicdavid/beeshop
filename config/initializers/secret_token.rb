79fbc7327ededd2a63fe9fabd82d5bd503adfcc3
David Rodriguez <stoicdavid@gmail.com>
2013-09-09 00:53:23 -0500

    Inicializar el repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beeshop::Application.config.secret_key_base = '2edc52dcf145efa4c40fbdcefac0366e26e838fe421dc0db07c9883e73af8281efda254d0682596faf7cd3e1e18ff374af25663ac8ca997ffc4bf8de3ee2f38b'
