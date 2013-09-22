c0cb6c2def964596dc14c85a4f55b8ef353f13fd
David Rodriguez <stoicdavid@gmail.com>
2013-09-22 00:52:44 -0500

    notices in bootstrap added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  
+  validates :name, :description, :image_url, :category_ref, presence: true 
+  validates :price, numericality: {greater_than_or_equal_to: 0.01} 
+  validates :name, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+      with:    %r{\.(gif|jpg|png)\Z}i,
+  message: 'must be a URL for GIF, JPG or PNG image.' }
 end
